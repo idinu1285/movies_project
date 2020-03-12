eb012727c8b357d8789e6682898adf649390f280
Dinu I. Iulian <49760637+idinu1285@users.noreply.github.com>
2020-03-12 16:09:53 +0200

    Add files via upload

---
+++ output/output.py
@@ -0,0 +1,71 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\user\Desktop\movies_project-master\movies_project-master\final_project.py 
+
+        :return: prints a dictionary with frequency of duplicates of market movies countries(key : value)
+        
+The distribution of market countries duplicates is: {'United States of America': 62, 'Taiwan': 1, 'Hong Kong': 1, 'China': 1, 'Australia': 1, 'Russia': 1, 'Poland': 1, 'Estonia': 2, 'Romania': 2, 'Jamaica': 1, 'France': 2, 'United Kingdom': 5, 'Worldwide': 1, 'Germany': 2, 'Japan': 1, 'South Africa': 1, 'Rwanda': 1, 'Serbia': 2, 'Italy': 2, 'Norway': 1, 'Sweden': 1, 'Ukraine': 2, 'New Zeeland': 2, 'India': 1, 'Spain': 2, 'Mexico': 1}
+=====================================================================================================
+
+        :return: prints a dictionary with frequency of duplicates of production studios(key : value)
+        
+The distribution of production studios duplicates is: {'Hal Roach Studios': 1, 'Warner Bros.': 8, 'Columbia Pictures': 5, 'Media Asia Films': 1, 'Beijing Film Studio': 1, 'The South Australian Film Co.': 1, 'Mosfilm': 1, '20th Century Fox': 5, 'Paramount Pictures': 8, 'Orion Pictures': 1, 'Zespol ': 1, 'Pacific Western': 1, 'AllFilm': 1, 'Mobra Films': 1, 'Big Dog Films': 1, 'Carolco Pictures': 1, 'Studioul Cinematografic Buc.': 1, 'Taska Production': 1, 'Marvel Studios': 1, 'Canal +': 1, 'Desilu Productions': 1, 'Momentum Pictures': 1, 'Bali Film Center': 1, 'New Line Cinema': 2, 'Merlin Films': 1, 'Crackin Films': 1, 'Walt Disney Pictures': 5, 'Hughes Entertainment': 2, 'DreamWorks': 1, 'Schiwago Films': 1, 'Basara Pictures': 1, 'Universal Pictures': 5, 'Castle Rock Entertainment': 1, ' Produzioni Europee Associate': 2, 'Lucasfilm': 1, 'M6 Films': 1, 'Film Afrika Worldwide': 1, 'Ironwood Films': 1, 'Pathe Productions': 1, 'CiBy 2000': 1, 'Rasta Films': 1, 'Melampo Cinematografica': 1, 'Filmeffekt As': 1, 'Yellow Bird': 1, 'Kinorob': 1, 'Pinewood Studios': 1, 'Limelite': 1, 'Bona Film Group': 1, 'Dream Team Films': 1, 'Miramax Films': 1, 'Icon Productions': 1, 'Alcina Pictures': 1, 'Excel Entertainment': 1, 'United Artists': 1, 'Estudios Churubusco Azteca': 1, 'Charttoff - Winkler Productions': 1, 'BRON Studios': 1, 'Walt Disney Animation Studios': 1, 'APT Studios': 1, 'ERBP': 1, 'Harvest Filmworks': 1, 'WingNut Films': 1, 'Decla - Bioscop AG': 1, 'Metro - Goldwyn - Mayer': 1, 'Altavista Films': 1, 'RKO Radio Pictures': 1, 'Wild Pictures': 1}
+=====================================================================================================
+
+       :return: prints a list with the titles of movies with estimated budgets within in interval 100000000 and 250000000 USD
+       
+The movies titles with estimated budget between 100mil and 250mil USD are: ['Titanic ', 'Terminator 2: Judgement Day', 'Teminator Genesys', 'Shazam!', 'Star Trek', 'Pirates of Caribbean: Dead Men Tell No Tales', "Pirates of Caribbean:Dead Man's Chest", 'Django Unchained', 'King Kong', 'The Aviator', 'Wateworld', 'John Carter', 'Spider - Man 2', 'X -Men: The Last Stand', 'The Lego Movie 2: The Second Part', 'Aladdin']
+=====================================================================================================
+
+        :return: prints a list with release year of movies which have as main role actor Al Pacino or which are directed
+        by Bradley Cooper
+        
+Production years of movies which have as main role actor A. Pacino or director as B. Cooper: [1995, 2018]
+=====================================================================================================
+
+        :return: prints a list with directors names which direct movies with lower or highest rating
+        
+The names of directors of movies which directed the lower or the higher rated movie: ['Quentin Tarantino', 'Jeff Renfroe']
+=====================================================================================================
+
+        :return: prints a list with actors names which act in shortest or the longest movie
+        
+The names of main role actors which act in the shortest or the longest movie as the duration: ['Clark Gable', 'Dan Castellaneta']
+=====================================================================================================
+
+        :return: prints a list with genres of movies produced in Romania or France
+        
+The genres of movies produced in Romania or France are: ['Drama', 'History', 'Biography', 'Crime']
+=====================================================================================================
+
+        :return: prints a list with durations of movies released in only odd year after 2015
+        
+The durations of movies released after 2015 in odd years: [181, 132, 129, 92, 159, 92, 153, 107, 128] (minutes)
+=====================================================================================================
+
+        :return: prints a list with estimated budgets of movies wich have rating > 9.5 or runtime > 200 minutes
+        
+The budgets of movies with IMDB ratings grater than 9.5 or durations grater than 200 min:[3977000.0, 15000.0, 700000.0, 93000000.0, 13000000.0, 95000000.0](USD)
+=====================================================================================================
+
+        :return: prints a list with languages of movies produced in Canal + or 20th Century Fox studios
+        
+The language of movies produced by Canal + or 20th Century Fox: ['English', 'French', 'English', 'English', 'English', 'English']
+=====================================================================================================
+
+        :return: prints a list with IMDB ratings of movies The Godfather or Star Trek
+        
+The IMDB rating of THE GODFATHER or STAR TREK movie is: [8.0, 9.2]
+=====================================================================================================
+
+        :return: prints a list with titles of movies that are of art type
+        
+The titles of art movies are: ['Andrei Rublev', 'Titanic ', 'RoboCop', 'Mihai Viteazul', 'Avengers Endgame', 'Shazam!', 'Star Trek', 'Star Trek(TV Series 1966-1969)', 'The Lord Of The Ring: The Return of the King', 'Pirates of Caribbean: Dead Men Tell No Tales', "Pirates of Caribbean:Dead Man's Chest", 'Save The Private Ryan', 'The Godfather', 'The Godfather - Part II', 'The Good the Bad and the Ugly', 'Forrest Gump', 'Django Unchained', 'Once Upon a Time..In Hollywood', 'King Kong', 'The Aviator', 'Rocky III', 'Rambo: First Blood Part II', 'Rocky ', 'Little Ashes', 'Spider - Man 2', 'X -Men: The Last Stand', 'Casablanca', 'A Star Is Born', 'Wild Oats']
+=====================================================================================================
+
+        :return: prints a list with titles of movies that are of commercial type
+        
+The titles of commercial movies are: ['Sons Of Dessert', 'Gone With The Wind', 'Crounching Tiger, Hidden Dragon', 'Internal Affairs', 'Red Cliff', 'Wolf Creek', 'Duck Soup', 'Going My Way', 'Dennis The Menace', 'Teddy Bear', 'The Terminator', 'Tangerines', '4 Months, 3 Weeks and 2 Days', 'Belly', 'Terminator 2: Judgement Day', 'Punane Elavhobe', 'Teminator Genesys', 'The Intouchables', 'Wilderness', 'The Simpsons (TV Series 1988-)', 'Samsara', 'Heat', 'The General', 'Owned & Operated', 'Alladin', 'Home Alone', 'Home Alone 2: Lost in New york', "Bram Stocker's Dracula", 'A Coffee in Berlin', 'Audition', 'Ted ', 'The Shawshank Redemption', 'Star Wars V: The Empire Strikes Back', 'Tell No One', 'Invictus', 'In My Country', 'Winnie Mandela', 'Africa United', 'White Cat, Black Cat', 'Life Is a Miracle', 'Life is Beautiful', 'For a Few Dollars More', 'Herman', 'The Girl Who Played with Fire', 'Battle for Sevastopol', 'A Countess from Hong Kong', 'Homeward', 'We Die Young', 'Apocalypto', 'The Colony', 'Die Hard', 'Rush Hour', 'Gully Boy', 'The Birth of A Nation', 'Wateworld', 'Johnny English Strikes Again', 'Tangled', 'John Carter', 'Johnny English', 'Primer', 'Pi', 'Bad Taste', 'The Cabinet of Dr. Caligari', 'El Mariachi', 'The Good Earth', 'All About Eve', 'To Catch A Thief', 'Amores Perros', 'I Remember Mama', 'The Lego Movie 2: The Second Part', 'Aladdin']
+=====================================================================================================
+>>> 
